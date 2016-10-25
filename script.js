b95ca69401e91a7085d8ed9790bbd34272e41478
Spatzerny <spatzerny@gmail.com>
2016-10-25 23:23:12 +0200

    because things need resons

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var btn_boop001 = document.getElementById('btn_boop001'),
-	aud_boop001 = document.getElementById('aud_boop001');
+	aud_boop001 = document.getElementById('aud_boop001'),
+	counter_boop001 = 0;
 
 btn_boop001.addEventListener('click', function(){
 	aud_boop001.currentTime = 0;
 	aud_boop001.play();
+	counter_boop001++;
+	btn_boop001.innerHTML = counter_boop001;
 })
